80ae88798b4997901a52a5be3d3243d3cd502251
Austin <au.sophonsri@gmail.com>
2021-02-11 15:45:10 -0800

    1-5 done

--- components/product-item.js
+++ components/product-item.js
@@ -2,6 +2,67 @@
 
 class ProductItem extends HTMLElement {
   // TODO
+  constructor(title, item_price, img_src, id, state)
+  {
+    // default init
+    super();
+    const shadowRoot = this.attachShadow({mode: 'open'});
+
+    this.generate_item_attributes(title, item_price, img_src, id, state, shadowRoot);
+  }
+
+  generate_item_attributes(title, item_price, img_src, id, state, shadow) {
+
+    // initialize the item wrapper
+    const item = document.createElement('li');
+    item.setAttribute('class', 'product');
+
+
+    // initialize item attr. objects
+    let img = document.createElement("img");
+    let label = document.createElement("p");
+    let price = document.createElement("p");
+    let button = document.createElement("button");
+
+    // set image attributes
+    img.setAttribute('width', 200);
+    img.setAttribute('src', img_src);
+    img.setAttribute('alt', title);
+
+    // set label attributes
+    label.setAttribute('class', 'title');
+    label.innerHTML = title;
+
+    // set price attributes
+    price.setAttribute('class', 'price');
+    price.innerHTML = '$' + item_price;
+
+    // set button attributes
+    button.setAttribute('onclick', 'alert(\'Added to Cart!\')');
+    button.innerHTML = 'Add to Cart';
+    button.addEventListener('click', update_button);
+
+    // insert substructures to wrapper
+    item.appendChild(img);
+    item.appendChild(label);
+    item.appendChild(price);
+    item.appendChild(button);
+
+    // link external stylesheet
+    const linkElem = document.createElement('link');
+    linkElem.setAttribute('rel', 'stylesheet');
+    linkElem.setAttribute('href', './styles/styles.css');
+
+    shadow.appendChild(linkElem);
+    shadow.appendChild(item);
+
+  }
+
 }
+function update_button() {
+  console.log("button pressed");
+  this.innerHTML = "joe mama";
+}
+
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,51 @@
 // Script.js
+var localStorage = window.localStorage;
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  // let df = get_data();
+  // console.log(df);
+  // localStorage.setItem('df', df)
+  init();
 });
+
+async function init() {
+  await get_data();
+  await list_items();
+}
+
+async function get_data() {
+  let url = 'https://fakestoreapi.com/products';
+  try {
+    // wait for response and wait for json parse
+    let response = await fetch(url);
+    let df = JSON.stringify(await response.json());
+    // console.log(df);
+    // write to local storage
+    localStorage.setItem('df', df);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+async function list_items() {
+  var dataframe = JSON.parse(localStorage.getItem("df"));
+  dataframe.forEach(create_item);
+}
+
+function create_item(item_json) {
+  // console.log(item_json);
+  // access the catalogue wrapper
+  let catalogue = document.getElementById("product-list");
+
+  // initialize new item through ctor
+  let item = new ProductItem(
+    item_json.title,
+    item_json.price,
+    item_json.image,
+    item_json.id,
+    true);
+
+  // add item to catalogue
+  catalogue.appendChild(item);
+}
